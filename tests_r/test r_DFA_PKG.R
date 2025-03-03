c699b4ebbcb6ce6d3e04497eae8cec02f6efc267
Fernando Ferraz Ribeiro <ffribeiro@gmail.com>
2025-03-03 18:31:57 -0300

    R tests and comparisson

--- tests_r/test r_DFA_PKG.R
+++ tests_r/test r_DFA_PKG.R
@@ -1,24 +1,24 @@
 require("DFA")
 
-# tws scale
+# define time windows scales
 tws = c(4, 5 , 7, 9, 11, 13, 16, 20, 23, 28, 33, 38, 45, 52, 60, 69, 79, 91, 104, 119, 135, 154, 174, 198, 223, 252, 285, 321,
         362, 407, 457, 513, 575, 645, 723, 809, 905, 1011, 1130, 1261, 1407, 1570)
 
-
-print(length(tws))
+print("tws length",length(tws))
 
 # read data
 data <- read.csv("https://tinyurl.com/zbtestdata01")
 
+# extract series
 f3 <- data[, c("F3")]
 f6 <- data[, c("F6")]
 
 print("Start")
 startTime <- Sys.time() 
 
-result_rhoDCCA =  rhoDCCA(f3, f6, scale ="F", box_size= tws)
+result_DFA_PKG =  rhoDCCA(f3, f6, scale ="F", box_size= tws)
 
 endTime <- Sys.time()
 
-# prints recorded time 
+# print recorded time 
 print(endTime - startTime)
