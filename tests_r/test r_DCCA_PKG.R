8ac798436739adab74e97fe6252f7c35fa7e11a6
Fernando Ferraz Ribeiro <ffribeiro@gmail.com>
2025-03-03 18:22:43 -0300

    R tests and comparisson

--- tests_r/test r_DCCA_PKG.R
+++ tests_r/test r_DCCA_PKG.R
@@ -4,11 +4,11 @@ tws = c(4, 5 , 7, 9, 11, 13, 16, 20, 23, 28, 33, 38, 45, 52, 60, 69, 79, 91, 104
          362, 407, 457, 513, 575, 645, 723, 809, 905, 1011, 1130, 1261, 1407, 1570)
 
 data <- read.csv("https://tinyurl.com/zbtestdata01")
-
+print(length(tws))
 f3 <- data[, c("F3")]
 f6 <- data[, c("F6")]
 
-
+print("Start")
 startTime <- Sys.time() 
 
 result =  rhodcca(f3, f6, m = tws, nu = 0, overlap = TRUE)
