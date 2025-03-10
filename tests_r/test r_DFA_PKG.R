22c353f9410412ece85d0f0bda4d710df8e478bc
Fernando Ferraz Ribeiro <ffribeiro@gmail.com>
2025-03-09 23:13:59 -0300

    conclusion

--- tests_r/test r_DFA_PKG.R
+++ tests_r/test r_DFA_PKG.R
@@ -1,8 +1,11 @@
 require("DFA")
 
 # define time windows scales
-tws = c(4, 5 , 7, 9, 11, 13, 16, 20, 23, 28, 33, 38, 45, 52, 60, 69, 79, 91, 104, 119, 135, 154, 174, 198, 223, 252, 285, 321,
-        362, 407, 457, 513, 575, 645, 723, 809, 905, 1011, 1130, 1261, 1407, 1570)
+tws <- c(4, 5, 7, 9, 11, 13, 16, 20, 23, 28, 33,
+         38, 45, 52, 60, 69, 79, 91, 104, 119,
+         135, 154, 174, 198, 223, 252, 285, 321,
+         362, 407, 457, 513, 575, 645, 723, 809,
+         905, 1011, 1130, 1261, 1407, 1570)
 
 print(length(tws))
 
@@ -14,12 +17,12 @@ f3 <- data[, c("F3")]
 f6 <- data[, c("F6")]
 
 print("Start")
-startTime <- Sys.time() 
+start_time <- Sys.time()
 
-result_DFA_PKG =  rhoDCCA(f3, f6, scale ="F", box_size= tws)
+rhoDCCA(f3, f6, scale = "F", box_size = tws)
 
-endTime <- Sys.time()
+end_time <- Sys.time()
 
 # print recorded time
 print("DFA_PKG result: ")
-print(endTime - startTime)
+print(end_time - start_time)
--- tests_r/test_4_series_DCCA.R
+++ tests_r/test_4_series_DCCA.R
@@ -1,36 +1,27 @@
 require(combinat)
 require("DCCA")
-
 # define time windows scales
-tws = c(4, 5 , 7, 9, 11, 13, 16, 20, 23, 28, 33, 38, 45, 52, 60, 69, 79, 91, 104, 119, 135, 154, 174, 198, 223, 252, 285, 321,
-        362, 407, 457, 513, 575, 645, 723, 809, 905, 1011, 1130, 1261, 1407, 1570);
+tws <- c(4, 5, 7, 9, 11, 13, 16, 20, 23, 28, 33,
+         38, 45, 52, 60, 69, 79, 91, 104, 119,
+         135, 154, 174, 198, 223, 252, 285, 321,
+         362, 407, 457, 513, 575, 645, 723, 809,
+         905, 1011, 1130, 1261, 1407, 1570)
 print("Number of time windows scales:")
 print(length(tws))
-
-# load data
-data <- read.csv("https://tinyurl.com/zbtestdata01")
+data <- read.csv("https://tinyurl.com/zbtestdata01")# load data
 dcca_of <- combn(4, 2)
-
-
-
 result_DCCA_PKG <- list()
-startTime <- Sys.time()
-
-
+start_time <- Sys.time()
 for(i in 1:dim(dcca_of)[2]){
- 
-  
-  s1 <- data[[ dcca_of[,i ][1]]]
-  s2 <- data[[ dcca_of[,i ][2]]]
-  print(c(dcca_of[,i ][1], dcca_of[,i ][2]))
+  s1 <- data[[ dcca_of[, i][1]]]
+  s2 <- data[[ dcca_of[, i][2]]]
+  print(c(dcca_of[, i][1], dcca_of[, i][2]))
 
-  temp = rhodcca(s1, s2, m = tws, nu = 0, overlap = TRUE)
-  result_DCCA_PKG <- append(result_DCCA_PKG, temp, after = length(result_DCCA_PKG))
+  temp <- rhodcca(s1, s2, m = tws, nu = 1, overlap = TRUE)
+  result_DCCA_PKG <- append(result_DCCA_PKG, temp, 
+                            after = length(result_DCCA_PKG))
 }
-
-endTime <- Sys.time()
-
-# print recorded time
+end_time <- Sys.time()
 print("elapsed time:")
-elapsed_DCCA_PKG <- endTime - startTime
+elapsed_DCCA_PKG <- end_time - start_time
 print(elapsed_DCCA_PKG)
