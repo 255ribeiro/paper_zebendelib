0042557d7f2c401e3ce7d63b871378c715c6650b
Fernando Ferraz Ribeiro <ffribeiro@gmail.com>
2025-03-24 08:34:58 -0300

    calc

--- tests_r/test_n_series_DCCA.R
+++ tests_r/test_n_series_DCCA.R
@@ -6,13 +6,13 @@ tws <- c(4, 5, 7, 9, 11, 13, 16, 20, 23, 28, 33,
          135, 154, 174, 198, 223, 252, 285, 321,
          362, 407, 457, 513, 575, 645, 723, 809,
          905, 1011, 1130, 1261, 1407, 1570)
-cat("Number of time windows scales: ", length(tws)
+cat("Number of time windows scales: ", length(tws), "\n")
 series_count <- 12 # select the number of series to run 
 data <- read.csv("https://tinyurl.com/zbtestdata64c")# load data
 data <- data[, 1:series_count] # filter columns
 start_time <- Sys.time() # start time stamp
 dcca_of <- combn(c(1:series_count), 2)
-cat("Number of series:", length(data)
+cat("Number of series:", length(data), "\n")
 result_DCCA_PKG <- list()
 for(i in 1:dim(dcca_of)[2]){
   s1 <- data[[ dcca_of[, i][1]]] # selecting first seires
@@ -23,4 +23,4 @@ for(i in 1:dim(dcca_of)[2]){
 }
 end_time <- Sys.time() #end time stamp
 elapsed_DCCA_PKG <- end_time - start_time #elapsed time
-cat("elapsed time: ", elapsed_DCCA_PKG)
+cat("elapsed time: ", elapsed_DCCA_PKG, "\n")
