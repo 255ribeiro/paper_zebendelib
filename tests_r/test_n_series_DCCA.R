0f4ff06ed3506f62157a0d4220967135ead99ee5
Fernando Ferraz Ribeiro <ffribeiro@gmail.com>
2025-03-24 08:53:30 -0300

    update tests

--- tests_r/test_n_series_DCCA.R
+++ tests_r/test_n_series_DCCA.R
@@ -7,7 +7,7 @@ tws <- c(4, 5, 7, 9, 11, 13, 16, 20, 23, 28, 33,
          362, 407, 457, 513, 575, 645, 723, 809,
          905, 1011, 1130, 1261, 1407, 1570)
 cat("Number of time windows scales: ", length(tws), "\n")
-series_count <- 12 # select the number of series to run 
+series_count <- 4 # select the number of series to run 
 data <- read.csv("https://tinyurl.com/zbtestdata64c")# load data
 data <- data[, 1:series_count] # filter columns
 start_time <- Sys.time() # start time stamp
@@ -15,8 +15,8 @@ dcca_of <- combn(c(1:series_count), 2)
 cat("Number of series:", length(data), "\n")
 result_DCCA_PKG <- list()
 for(i in 1:dim(dcca_of)[2]){
-  s1 <- data[[ dcca_of[, i][1]]] # selecting first seires
-  s2 <- data[[ dcca_of[, i][2]]] # selecting second seires
+  s1 <- data[[ dcca_of[, i][1]]] # selecting first series
+  s2 <- data[[ dcca_of[, i][2]]] # selecting second series
   temp <- rhodcca(s1, s2, m = tws, nu = 1, overlap = TRUE) # Pdcca calculations
   result_DCCA_PKG <- append(result_DCCA_PKG, temp,
                             after = length(result_DCCA_PKG)) # collecting results
--- tests_r/test_n_series_DFA.R
+++ tests_r/test_n_series_DFA.R
@@ -7,15 +7,15 @@ tws <- c(4, 5, 7, 9, 11, 13, 16, 20, 23, 28, 33,
          362, 407, 457, 513, 575, 645, 723, 809,
          905, 1011, 1130, 1261, 1407, 1570)
 cat("Number of time windows scales: ",length(tws), "\n")
-series_count <- 12 # select the number of series to run 
+series_count <- 4 # select the number of series to run
 data <- read.csv("https://tinyurl.com/zbtestdata64c")# load data
 data <- data[, 1:series_count] # filter columns
 start_time <- Sys.time() # time stamp
 dcca_of <- combn(c(1:series_count), 2)
 cat("Number of series: ", length(data), "\n")
 for (i in 1:dim(dcca_of)[2]){
-  s1 <- data[[ dcca_of[, i][1]]] # selecting first seires
-  s2 <- data[[ dcca_of[, i][2]]] # selecting second seires
+  s1 <- data[[ dcca_of[, i][1]]] # selecting first series
+  s2 <- data[[ dcca_of[, i][2]]] # selecting second series
 
   rhoDCCA(s1, s2, scale = "F", box_size = tws) # Pdcca calculations
 }
